bca7b41495bcbf0c8c659550f41907eacdddf709
Fernando Cladera <fcladera@fcladera.com>
2014-07-11 12:20:08 -0300

    Primer draft, botones css

--- website/scripts.js
+++ website/scripts.js
@@ -1,3 +1,29 @@
+//------------------------------------------------
+//-------- JQuery scripts
+
+$(document).ready(function(){
+	$('#button10').on('click', function(){
+		sendmessage(1);
+		$(this).toggleClass('on');
+	});
+	$('#button20').on('click', function(){
+		sendmessage(2);
+		$(this).toggleClass('on');
+	});
+	$('#button30').on('click', function(){
+		sendmessage(3);
+		$(this).toggleClass('on');
+	});
+	$('#button40').on('click', function(){
+		sendmessage(4);
+		$(this).toggleClass('on');
+	});
+	Conect();
+});
+
+//------------------------------------------------
+//-------- WebSocket scripts
+
 var ip = location.host;
 //alert(ip);
 
@@ -24,15 +50,20 @@ function onMessage(evt){
 	var aux
 	var chaine = evt;
 	chaine = chaine.split(':');
-	aux = 'div'+chaine[0]+'0';
+	aux = 'button'+chaine[0]+'0';
 	print = document.getElementById(aux);	
 	//print.innerHTML = chaine[1];
-	if (chaine[1] == 'OFF') {print.innerHTML ='<span style="color: red;">OFF</span>' }
-	else {print.innerHTML = '<span style="color: green;">ON</span>'}
-		
-	
+	if (chaine[1] == 'OFF'){
+		$(print).removeClass('ison');
+		//print.innerHTML ='<span style="color: red;">OFF</span>' 
+	}
+	else {
+		$(print).addClass('ison');
+		//print.innerHTML = '<span style="color: green;">ON</span>'
+	}
 }
 function sendmessage(id){			
 	conect.send(id);
 }
-	
+
+
