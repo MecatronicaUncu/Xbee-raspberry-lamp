ec8953a24d71079bd23581ac2317bf3b0c486bd2
Fernando Cladera <fcladera@fcladera.com>
2014-07-13 01:17:58 -0300

    Los pulsadores cambian de estado segun los mensajes WebSocket.

--- website/scripts.js
+++ website/scripts.js
@@ -1,12 +1,16 @@
 //------------------------------------------------
 //-------- JQuery scripts
 
+// Array con el estado de los botones
+var buttonStateArray = new Array(false,false, false, false);
+
 $(document).ready(function(){
 		
 	$('#button10').on('click', function(){
 		if(isConnected){
 			sendmessage(1);
 			$(this).toggleClass('on');
+			buttonStateArray[0] = !buttonStateArray[0];
 		}
 		else{
 			alert('Debe conectarse para utilizar los interruptores');
@@ -16,6 +20,7 @@ $(document).ready(function(){
 		if(isConnected){
 			sendmessage(2);
 			$(this).toggleClass('on');
+			buttonStateArray[1] = !buttonStateArray[1];
 		}
 		else{
 			alert('Debe conectarse para utilizar los interruptores');
@@ -25,6 +30,7 @@ $(document).ready(function(){
 		if(isConnected){
 			sendmessage(3);
 			$(this).toggleClass('on');
+			buttonStateArray[2] = !buttonStateArray[2];
 		}
 		else{
 			alert('Debe conectarse para utilizar los interruptores');
@@ -34,6 +40,7 @@ $(document).ready(function(){
 		if(isConnected){
 			sendmessage(4);
 			$(this).toggleClass('on');
+			buttonStateArray[3] = !buttonStateArray[3];
 		}
 		else{
 			alert('Debe conectarse para utilizar los interruptores');
@@ -77,8 +84,14 @@ function Connect() {
 }
 
 // ConectWebSocket, crea websocket. Callbacks.
-function ConectWebSocket() { 
-	conect = new WebSocket(wsUri); 
+function ConectWebSocket() {
+	try{
+		conect = new WebSocket(wsUri);
+	}
+	catch(err){
+		console.log("Error creando WebSocket: "+err);
+		return false;
+	}
 	conect.onopen = function(evt) {
 		output.innerHTML = '<span style="color: white;">CONECTADO</span>';
 		sendmessage('E');
@@ -107,30 +120,43 @@ function Disconnect(){
 function onMessage(evt){
 	var cadena = evt;
 	var msgs = cadena.split(';');
-	for(var i=0; i< msgs.length;i++){
-		//alert(msgs[i]);
+	for(var i=0; i< msgs.length-1;i++){
 		var chaine = msgs[i].split(':');
 		//alert(chaine[0])
-		switch(chaine[0]){
+		var buttonId = chaine[0];
+		var buttonState = chaine[1];
+		switch(buttonId){
 			case '1':
 			case '2':
 			case '3':
 			case '4':
 				//alert("Buena Cadena "+chaine[0]+"   "+chaine[1]);
-				var aux = 'button'+chaine[0]+'0';
+				var aux = 'button'+buttonId+'0';
 				var button = document.getElementById(aux);	
 				//print.innerHTML = chaine[1];
-				if (chaine[1] == 'OFF'){
+				if(buttonState == 'OFF'){
 					$(button).removeClass('ison');
-					//button.innerHTML ='<span style="color: red;"></span>' 
+					//button.innerHTML ='<span style="color: red;"></span>'
+					// Cambiar el estado del boton tambien si no ha sido cambiado
+					if(buttonStateArray[buttonId-1]==true){
+						$('#button'+buttonId+'0').removeClass('on');
+						buttonStateArray[buttonId-1] = false;
+					}
 				}
-				else {
+				else if(buttonState == 'ON'){
 					$(button).addClass('ison');
 					//button.innerHTML = '<span style="color: green;">ON</span>'
+					if(buttonStateArray[buttonId-1]==false){
+						$('#button'+buttonId+'0').addClass('on');
+						buttonStateArray[buttonId-1] = true;
+					}
+				}
+				else{
+					console.log("Mala cadena (else): "+buttonId+":"+buttonState);
 				}
 				break;
 			default:
-				//alert("Mala cadena");
+				console.log("Mala cadena (default): "+buttonId+":"+buttonState);
 				break;
 		}
 			
@@ -145,7 +171,7 @@ function sendmessage(id){
 }
 
 // Función de test, cambia el estado de isConnected
-function test(){
+function toggleIsConnected(){
 	if(isConnected)
 		setConnected(false);
 	else
